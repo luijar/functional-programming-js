cbab09d003c9eff98b8073cb9e504c30b95cdf46
luijar <luijar@luisatencio.net>
2016-08-04 23:31:41 -0300

    Fix/update chapter 1 code

--- src2/ch01/listing1_7/listing1_7.js
+++ src2/ch01/listing1_7/listing1_7.js
@@ -1,8 +1,7 @@
 Rx.Observable.fromEvent(document.querySelector('#student-ssn'), 'keyup')
-  .map(input => input.srcElement.value)
-  .filter(ssn => ssn !== null && ssn.length !== 0)
+  .pluck('srcElement', 'value')
   .map(ssn => ssn.replace(/^\s*|\s*$|\-/g, ''))
-  .skipWhile(ssn => ssn.length !== 9)
+  .filter(ssn => ssn !== null && ssn.length === 9)
   .subscribe(validSsn => {
      console.log(`Valid SSN ${validSsn}`);
 });
