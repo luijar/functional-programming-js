22b742cc92a3dfdaf1e150c0ec38d5e20b81dc65
luijar <luijar@luisatencio.net>
2016-08-09 12:29:42 -0400

    Fix/update Chapter 4 code

--- src2/ch04/tests.js
+++ src2/ch04/tests.js
@@ -12,6 +12,8 @@ const R = require('ramda');
 // Globally used functions throughout all code listings
 const isEmpty = s => !s || !s.trim();
 const isValid = val => !_.isUndefined(val) && !_.isNull(val);
+const trim = (str) => str.replace(/^\s*|\s*$/g, '');
+const normalize = (str) => str.replace(/\-/g, '');
 
 QUnit.test("Chaining methods together", function () {
 
@@ -29,3 +31,28 @@ QUnit.test("Chaining methods together", function () {
 	assert.deepEqual(result, [ 'Alonzo Church', 'Haskell Curry', 'John Von Neumann', 'Stephen Kleene' ]);	
 });
 
+QUnit.test("Check Type tests", function () {
+
+	const checkType = require('./helper').checkType;	
+	assert.equal(checkType(String)('Curry'), 'Curry');
+	assert.equal(checkType(Number)(3), 3);
+	let now = new Date();
+	assert.equal(checkType(Date)(now), now);
+	assert.deepEqual(checkType(Object)({}), {});	
+	assert.throws(() => {
+		checkType(String)(42)
+	}, TypeError);
+});
+
+QUnit.test("Tuple test", function () {
+
+	const Tuple = require('./helper').Tuple;	
+	const StringPair = Tuple(String, String);
+	const name = new StringPair('Barkley', 'Rosser');
+	let [first, last] = name.values();  // In Node you need to use let
+	assert.equal(first, 'Barkley');
+	assert.equal(last, 'Rosser');
+	assert.throws(() => {
+		const fullname = new StringPair('J', 'Barkley', 'Rosser');	
+	}, TypeError);	
+});
