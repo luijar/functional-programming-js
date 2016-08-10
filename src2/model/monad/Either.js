8e58eee8e034f6f048bea48e79340b2b518c85af
luijar <luijar@luisatencio.net>
2016-08-10 00:47:03 -0400

    Update/Fix CH05 code

---
+++ src2/model/monad/Either.js
@@ -0,0 +1,86 @@
+/**
+ * ES6 versions of Either monad used in FP in JS
+ * Author: Luis Atencio
+ */ 
+exports.Either = class Either {
+	constructor(value) {
+		this._value = value;
+	}
+	
+	get value() {
+		return this._value;
+	}
+	
+	static left(a) {
+		return new exports.Left(a);
+	}
+	
+	static right(a) {
+		return new exports.Right(a);
+	}
+	
+	static fromNullable(val) {
+		return val !== null ? Either.right(val): Either.left(val);
+	}
+	
+	static of(a){
+		return Either.right(a);
+	}
+};
+
+exports.Left = class Left extends exports.Either {
+	map(_) {
+		return this; // noop
+	}
+	
+	get value() {
+		throw new TypeError("Can't extract the value of a Left(a).");
+	}
+	
+	getOrElse(other) {
+		return other;
+	}
+	
+	orElse(f) {
+		return f(this._value);
+	}
+	
+	chain(f) {
+		return this;
+	}
+	getOrElseThrow(a) {
+		throw new Error(a);
+	}
+	
+	filter(f) {
+		return this;
+	}
+	
+	toString() {
+		return `Either.Left(${this._value})`;
+	}
+};
+
+exports.Right = class Right extends exports.Either {
+	map(f) {
+		return exports.Either.of(f(this._value));
+	}
+	getOrElse(other) {
+		return this._value;
+	}
+	orElse() {
+		return this;
+	}
+	chain(f) {
+		return f(this._value);
+	}
+	getOrElseThrow(_) {
+		return this._value;
+	}
+	filter(f) {
+		return exports.Either.fromNullable(f(this._value) ? this._value : null);
+	}
+	toString() {
+		return `Either.Right(${this.value})`;
+	}
+};
